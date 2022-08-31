12cc8cf3a0a1344f61c69571f71afbb7131c0bab
ParkSuJeong74 <sujeong9158@gmail.com>
2022-08-31 21:26:34 +0900

    feat: 로그인시 회원 존재 여부 및 비밀번호 일치 여부 확인

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,14 +1,27 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Logger, Post } from '@nestjs/common';
+import { User } from 'src/users/entities/user.entity';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
+  private readonly logger = new Logger(`AuthController`);
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
   async login(@Body() loginDto: LoginDto) {
-    await this.authService.login(loginDto);
+    const user: User = await this.authService.login(loginDto);
+    this.logger.verbose(`login success!`);
+    return {
+      user: {
+        id: user.id,
+        email: user.email,
+        gender: user.gender,
+        age: user.age,
+        phone: user.phone,
+        role: user.role,
+      },
+    };
   }
 
   @Post('logout')
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,5 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import * as bcrypt from 'bcrypt';
 import { Repository } from 'typeorm';
 import { User } from '../users/entities/user.entity';
 import { LoginDto } from './dto';
@@ -10,10 +15,27 @@ export class AuthService {
     @InjectRepository(User) private readonly userRepository: Repository<User>,
   ) {}
 
-  async login(loginDto: LoginDto) {
-    const { email, password } = loginDto;
-    return 'login!';
+  async findUserByEmail(email: string): Promise<User> {
+    return await this.userRepository.findOne({ where: { email } });
   }
+
+  async login(loginDto: LoginDto): Promise<User> {
+    const { email, password } = loginDto;
+    const user: User = await this.findUserByEmail(email);
+    if (!user) throw new NotFoundException('회원이 존재하지 않습니다.');
+
+    const isPwMatching: boolean = await bcrypt.compare(password, user.password);
+    if (!isPwMatching)
+      throw new UnauthorizedException('비밀번호가 일치하지 않습니다.');
+
+    // 토큰 발급
+    await this.makeTokens();
+    // redis에 저장
+
+    return user;
+  }
+
+  async makeTokens() {}
 
   async logout() {
     // logout
