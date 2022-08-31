990e8856209598389d3f52a57efb23c8494da3f1
coldrain-f <intmain__@naver.com>
2022-08-31 22:07:13 +0900

    feat: update-post.dto.ts 생성

--- backend/src/posts/dto/update-post.dto.ts
+++ backend/src/posts/dto/update-post.dto.ts
@@ -1,4 +1,11 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreatePostDto } from './create-post.dto';
+import { IsNotEmpty, IsString } from 'class-validator';
 
-export class UpdatePostDto extends PartialType(CreatePostDto) {}
+export class UpdatePostDto {
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @IsString()
+  @IsNotEmpty()
+  content: string;
+}
