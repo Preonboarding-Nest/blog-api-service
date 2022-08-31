dc8e1d367cac33599109b6ec916f1684d02494a5
Songhareem <ignisilva92@gmail.com>
2022-08-31 22:23:57 +0900

    [#4][statistics 기능 추가 작업]feat: statistics 이벤트 및 리스너 추가

---
+++ backend/src/statistics/listeners/statistics-save.listener.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { OnEvent } from '@nestjs/event-emitter';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { EVENTS } from '../../commons/constants';
+import { Statistic } from '../entities/statistic.entity';
+import { StatisticsSaveEvent } from '../events/statistics-save.event';
+
+@Injectable()
+export class StatisticsListener {
+  constructor(
+    @InjectRepository(Statistic)
+    private readonly statisticsRepository: Repository<Statistic>,
+  ) {}
+
+  @OnEvent(EVENTS.STATISTICS_SAVE)
+  async handleStatisticsSaveEvent(event: StatisticsSaveEvent): Promise<void> {
+    const { path, method } = event;
+
+    await this.statisticsRepository.save(
+      this.statisticsRepository.create({
+        path,
+        method,
+      }),
+    );
+  }
+}
