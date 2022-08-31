32610f9d320e53c8f90ff3a3308d63e65696d6a7
ParkSuJeong74 <sujeong9158@gmail.com>
2022-08-31 21:28:16 +0900

    feat: passport, jwt 패키지 설치 및 종속성 추가

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../users/entities/user.entity';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    JwtModule.register({}),
+    PassportModule.register({}),
+  ],
   providers: [AuthService],
   controllers: [AuthController],
 })
