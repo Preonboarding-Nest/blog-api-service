435cbe105b5fa215fa4a05a82bf1a9f1a01f934f
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-08-31 17:12:39 +0900

    feat: user entity 생성

--- backend/src/users/entities/user.entity.ts
+++ backend/src/users/entities/user.entity.ts
@@ -1,1 +1,44 @@
-export class User {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { Gender, Role } from '../entities/enums';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column({ type: 'enum', name: 'gender', enum: Gender })
+  gender: Gender;
+
+  @Column()
+  age: number;
+
+  @Column()
+  phone: string;
+
+  @CreateDateColumn({ type: 'timestamp', name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamp', name: 'updated_at' })
+  updatedAt: Date;
+
+  @Column()
+  lastAccessedAt: Date;
+
+  @Column()
+  isDeleted: boolean;
+
+  @Column({ type: 'enum', name: 'role', enum: Role })
+  role: Role;
+}
