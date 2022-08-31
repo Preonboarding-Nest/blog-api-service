aa61bd7aae2b99a0236e4595dad4ca87d08c89b2
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-08-31 18:14:15 +0900

    feat: create user 기능 구현

--- backend/src/users/dto/create-user.dto.ts
+++ backend/src/users/dto/create-user.dto.ts
@@ -1,1 +1,29 @@
-export class CreateUserDto {}
+import {
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+} from 'class-validator';
+import { GENDER_ENUM } from '../entities/enums';
+
+export class CreateUserDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+
+  @IsEnum(GENDER_ENUM)
+  @IsNotEmpty()
+  gender: GENDER_ENUM;
+
+  @IsNumber()
+  age: number;
+
+  @IsString()
+  @IsNotEmpty()
+  phone: string;
+}
