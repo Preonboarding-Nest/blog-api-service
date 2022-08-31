99b012f916b99b8e392baafddc3bfa66e826fbf0
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-08-31 18:39:45 +0900

    feat: create user 기본 기능 구현

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -10,12 +10,17 @@ import {
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { ApiCreatedResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { User } from './entities/user.entity';
 
 @Controller('users')
+@ApiTags('Users API')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
+  @ApiOperation({ summary: '유저 생성 API', description: '유저를 생성합니다.' })
+  @ApiCreatedResponse({ description: '유저를 생성합니다.', type: User })
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.createUser(createUserDto);
   }
