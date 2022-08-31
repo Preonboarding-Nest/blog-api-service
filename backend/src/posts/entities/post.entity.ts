77ffaa73a544bfa02a4d68b996de052043038b10
coldrain-f <intmain__@naver.com>
2022-08-31 18:15:12 +0900

    feat: post entity 생성

--- backend/src/posts/entities/post.entity.ts
+++ backend/src/posts/entities/post.entity.ts
@@ -1,1 +1,25 @@
-export class Post {}
+import {
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class Post {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  content: string;
+
+  @CreateDateColumn({ type: 'timestamp', name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamp', name: 'updated_at' })
+  updatedAt: Date;
+}
