d3cab26b51fcc54bd27456664f2b7daf72600db6
Songhareem <ignisilva92@gmail.com>
2022-09-01 10:49:26 +0900

    [#5][JSned Format 적용]feat: all-exception filter 추가

---
+++ backend/src/filters/all-exception.filter.ts
@@ -0,0 +1,35 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { HttpAdapterHost } from '@nestjs/core';
+import { HttpStatusMessage } from '../commons/constants';
+import { Response } from '../commons/interfaces/response.interface';
+
+@Catch()
+export class AllExceptionsFilter implements ExceptionFilter {
+  constructor(private readonly httpAdapterHost: HttpAdapterHost) {}
+
+  catch(exception: unknown, host: ArgumentsHost): void {
+    // In certain situations `httpAdapter` might not be available in the
+    // constructor method, thus we should resolve it here.
+    const { httpAdapter } = this.httpAdapterHost;
+
+    const ctx = host.switchToHttp();
+
+    const httpStatus =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    const responseBody: Response<void> = {
+      status: 'error',
+      message: HttpStatusMessage[httpStatus],
+    };
+
+    httpAdapter.reply(ctx.getResponse(), responseBody, httpStatus);
+  }
+}
