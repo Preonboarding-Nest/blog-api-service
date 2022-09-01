16f0ebcb7097535c1aec85df3e2a4e7246556fdf
coldrain-f <intmain__@naver.com>
2022-09-01 12:26:11 +0900

    feat: FindPostResponseDto에 Swagger 예시와 설명 추가

--- backend/src/posts/dto/find-post.dto.ts
+++ backend/src/posts/dto/find-post.dto.ts
@@ -1,11 +1,39 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Post } from '../entities/post.entity';
 
 export class FindPostResponseDto {
+  @ApiProperty({
+    example: 'olaf@naver.com',
+    description: '해당 필드는 게시글 등록자의 이메일입니다.',
+  })
   email: string;
+
+  @ApiProperty({
+    example: 1,
+    description: '해당 필드는 게시글의 고유 번호입니다.',
+  })
   id: number;
+
+  @ApiProperty({
+    example: '오라클 SQL과 PL/SQL을 다루는 기술',
+    description: '해당 필드는 게시글의 제목입니다.',
+  })
   title: string;
+
+  @ApiProperty({
+    example: '해당 보드를 클릭하고...',
+    description: '해당 필드는 게시글의 내용입니다.',
+  })
   content: string;
+
+  @ApiProperty({
+    description: '해당 필드는 게시글의 등록일입니다.',
+  })
   createdAt: Date;
+
+  @ApiProperty({
+    description: '해당 필드는 게시글의 수정일입니다.',
+  })
   updatedAt: Date;
 
   of(post: Post) {
