d7ff07efb56a11a359bf417a986184eb49da5665
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-09-02 11:16:20 +0900

    test: users service test 환경 설정

--- backend/src/users/users.service.spec.ts
+++ backend/src/users/users.service.spec.ts
@@ -1,18 +1,73 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { AuthService } from 'src/auth/auth.service';
+import { RedisService } from 'src/redis/redis.service';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
+
+const mockUserRepository = () => ({
+  create: jest.fn().mockImplementation((user) => user),
+  save: jest.fn().mockImplementation((user) => user),
+  find: jest.fn(),
+  findOne: jest.fn(),
+});
+
+const mockAuthService = () => ({});
+const mockRedisService = () => ({
+  delKey: jest.fn(),
+});
 
 describe('UsersService', () => {
   let service: UsersService;
+  let userRepository: Repository<User>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: mockUserRepository(),
+        },
+        {
+          provide: AuthService,
+          useValue: mockAuthService(),
+        },
+        {
+          provide: RedisService,
+          useValue: mockRedisService(),
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
+    userRepository = module.get(getRepositoryToken(User));
+  });
+
+  afterEach(async () => {
+    jest.clearAllMocks();
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  // it('creates a new user with a salted and hashed password', async () => {
+  //   const users: User[] = [];
+
+  //   const password = 'tester123!';
+  //   const userInfo: CreateUserDto = {
+  //     email: 'test@test.com',
+  //     password,
+  //     phone: '01012341234',
+  //     age: 28,
+  //     gender: GENDER_ENUM.MALE,
+  //   };
+  //   const user = await service.createUser(userInfo);
+
+  //   console.log('user', user);
+  //   console.log(users);
+  //   expect(user.password).not.toEqual(password);
+  // });
 });
