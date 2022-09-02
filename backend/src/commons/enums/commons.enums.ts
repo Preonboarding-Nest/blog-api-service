969b9b8f448c808e666be1a79ca7cacc4f1070d0
coldrain-f <intmain__@naver.com>
2022-09-02 10:11:41 +0900

    feat: 공통 ENUM에 게시판 타입 ENUM 추가

--- backend/src/commons/enums/commons.enums.ts
+++ backend/src/commons/enums/commons.enums.ts
@@ -4,3 +4,14 @@ export enum HTTP_METHOD_ENUM {
   PATCH = 'patch',
   DELETE = 'delete',
 }
+
+/**
+ * 1: 자유 게시판
+ * 2: 공지 게시판
+ * 3: 운영 게시판
+ */
+export enum POST_TYPE_ENUM {
+  FREE = 1,
+  NOTICE = 2,
+  PROD = 3,
+}
