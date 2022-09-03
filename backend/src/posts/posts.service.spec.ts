286211aee19056292dc2f4d8acef501d71fee74f
coldrain-f <intmain__@naver.com>
2022-09-03 16:57:42 +0900

    test: PostsService 단위 테스트를 위한 Mock 의존성 주입 설정

--- backend/src/posts/posts.service.spec.ts
+++ backend/src/posts/posts.service.spec.ts
@@ -1,12 +1,31 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { User } from '../users/entities/user.entity';
+import { Repository } from 'typeorm';
 import { PostsService } from './posts.service';
+import { PostCategory } from './entities/post-category.entity';
+import { Post } from './entities/post.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 describe('PostsService', () => {
   let service: PostsService;
 
+  // (1)
+  const mockPostRepository = () => ({});
+  const mockUserRepository = () => ({});
+  const mockPostCategoryRepository = () => ({});
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PostsService],
+      providers: [
+        PostsService,
+        // (2)
+        { provide: getRepositoryToken(Post), useValue: mockPostRepository() },
+        { provide: getRepositoryToken(User), useValue: mockUserRepository() },
+        {
+          provide: getRepositoryToken(PostCategory),
+          useValue: mockPostCategoryRepository(),
+        },
+      ],
     }).compile();
 
     service = module.get<PostsService>(PostsService);
