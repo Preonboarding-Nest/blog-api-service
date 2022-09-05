4ea762a2eb3c85c49ef35c039816fd77e9f7f5bc
Songhareem <ignisilva92@gmail.com>
2022-09-05 10:57:35 +0900

    feat: find statistics 기능 추가

--- backend/src/statistics/statistics.controller.ts
+++ backend/src/statistics/statistics.controller.ts
@@ -1,12 +1,52 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import {
+  ApiCookieAuth,
+  ApiCreatedResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
+import { GENDER_ENUM, ROLE_ENUM } from '../users/entities/enums';
+import { GetCurrentUserId } from '../commons/decorators';
 import { StatisticsService } from './statistics.service';
+import { TERM_ENUM } from '../commons/enums/commons.enums';
 
+@ApiTags('Statistics API')
 @Controller('statistics')
 export class StatisticsController {
   constructor(private readonly statisticsService: StatisticsService) {}
 
+  @ApiOperation({
+    summary: '통계 조회 API',
+    description: '통계 정보를 조회합니다.',
+  })
+  @ApiCreatedResponse({ description: '통계 정보를 조회합니다.' })
+  @UseGuards(AuthGuard('jwt'))
+  @ApiCookieAuth('accessToken')
   @Get()
-  find() {
-    return this.statisticsService.find();
+  async find(
+    @Query('from') from: string,
+    @Query('to') to: string,
+    @Query('resource') resource: string,
+    @Query('method') method: string,
+    @Query('gender') gender: GENDER_ENUM,
+    @Query('age_from') ageFrom: string,
+    @Query('age_to') ageTo: string,
+    @Query('user_role') user_role: ROLE_ENUM,
+    @Query('term') term: TERM_ENUM,
+    @GetCurrentUserId() currentUserId: number,
+  ) {
+    return await this.statisticsService.find({
+      from,
+      to,
+      resource,
+      method,
+      gender,
+      ageFrom,
+      ageTo,
+      user_role,
+      term,
+      currentUserId,
+    });
   }
 }
