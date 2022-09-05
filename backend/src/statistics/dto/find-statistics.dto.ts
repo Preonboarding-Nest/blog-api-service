aff2698f957d4cfdaa9226ee1eea476f5e8c0220
Songhareem <ignisilva92@gmail.com>
2022-09-05 10:56:39 +0900

    feat: find statistics dto 추가

---
+++ backend/src/statistics/dto/find-statistics.dto.ts
@@ -0,0 +1,15 @@
+import { TERM_ENUM } from 'src/commons/enums/commons.enums';
+import { GENDER_ENUM, ROLE_ENUM } from 'src/users/entities/enums';
+
+export class FindStatisticsDto {
+  from: string;
+  to: string;
+  resource: string;
+  method: string;
+  gender: GENDER_ENUM;
+  ageFrom: string;
+  ageTo: string;
+  user_role: ROLE_ENUM;
+  term: TERM_ENUM;
+  currentUserId: number;
+}
