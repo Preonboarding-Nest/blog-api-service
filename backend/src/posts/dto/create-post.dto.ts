8abe6f3e1c569a1809eb5d40c2403a160f23919d
Songhareem <ignisilva92@gmail.com>
2022-09-05 11:07:28 +0900

    feat: pull develop and solve conflict

--- backend/src/posts/dto/create-post.dto.ts
+++ backend/src/posts/dto/create-post.dto.ts
@@ -1,4 +1,6 @@
 import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { PostCategory } from '../entities/post-category.entity';
+import { Post } from '../entities/post.entity';
 
 export class CreatePostDto {
   @IsNumber()
@@ -12,4 +14,12 @@ export class CreatePostDto {
   @IsString()
   @IsNotEmpty()
   content: string;
+
+  toEntity(postCategory: PostCategory) {
+    const post = new Post();
+    post.title = this.title;
+    post.content = this.content;
+    post.postCategory = postCategory;
+    return post;
+  }
 }
--- backend/src/statistics/statistics.service.ts
+++ backend/src/statistics/statistics.service.ts
@@ -1,6 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { ROLE_ENUM } from '../users/entities/enums';
+import { UsersService } from '../users/users.service';
 import { Repository } from 'typeorm';
+import { FindStatisticsDto } from './dto/find-statistics.dto';
 import { Statistic } from './entities/statistic.entity';
 
 @Injectable()
@@ -8,9 +11,34 @@ export class StatisticsService {
   constructor(
     @InjectRepository(Statistic)
     private readonly statisticsRepository: Repository<Statistic>,
+    private readonly usersSerive: UsersService,
   ) {}
 
-  find() {
-    return `This action returns all statistics`;
+  async find(FindStatisticsDto: FindStatisticsDto) {
+    const {
+      from,
+      to,
+      resource,
+      method,
+      gender,
+      ageFrom,
+      ageTo,
+      user_role,
+      term,
+      currentUserId,
+    } = FindStatisticsDto;
+
+    // 시작시간
+    // 종료시간
+    // 년 월 일 시 분 초
+
+    const user = await this.usersSerive.findUserById(currentUserId);
+    if (user.role !== ROLE_ENUM.ADMIN) {
+      throw new ForbiddenException(
+        'Cannot find statistic with that permission',
+      );
+    }
+
+    return await `This action returns all statistics`;
   }
 }
